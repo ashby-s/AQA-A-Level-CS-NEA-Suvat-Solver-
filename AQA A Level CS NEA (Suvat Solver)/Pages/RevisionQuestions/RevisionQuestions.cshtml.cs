7694b1af9c3e698d93594c8790b65e49cef29c65
AshbyS <109519556+ashby-s@users.noreply.github.com>
2022-08-16 06:52:57 +0100

    Add more features and code to Revision Questions page

--- AQA A Level CS NEA (Suvat Solver)/Pages/RevisionQuestions/RevisionQuestions.cshtml.cs
+++ AQA A Level CS NEA (Suvat Solver)/Pages/RevisionQuestions/RevisionQuestions.cshtml.cs
@@ -9,8 +9,22 @@ namespace AQA_A_Level_CS_NEA__Suvat_Solver_.Pages.RevisionQuestions
     public class RevisionQuestionsModel : PageModel
     {
         public String Username { get; set; }
-        public int CorrectAnsw { get; set; }
-        public int TotalAnsw { get; set; }
+        public float Answ1 { get; set; } = 7;
+        public float Answ2 { get; set; }
+        public float Answ3 { get; set; }
+        public float Answ4 { get; set; }
+        public float Answ5 { get; set; }
+        public float Answ6 { get; set; }
+        public float QuestCorrectAnsw { get; set; }
+        //public int CorrectAnsw { get; set; }
+        //public int TotalAnsw { get; set; }
+        public bool AnswValid { get; set; }
+        public bool nextquestion { get; set; } = true;
+        public float BtnAnswer { get; set; }
+        public string Unit { get; set; }
+        //To show the solutions:
+        public int TempQuestId { get; set; }
+        public string TempQuestSolved { get; set; }
 
         private readonly ApplicationDbContext _context;
         public RevisionQuestionsModel(ApplicationDbContext context)
@@ -19,11 +33,31 @@ namespace AQA_A_Level_CS_NEA__Suvat_Solver_.Pages.RevisionQuestions
         }
         public void OnGet()
         {
-            Username = Username.Remove(0, 1);
             User User = new User();
             var CorrectUser = _context.User.FirstOrDefault(x => x.UserName == Username);
-            CorrectAnsw = CorrectUser.UserCorrectAnsw;
-            TotalAnsw = CorrectUser.UserTotalAnsw;
+            //CorrectAnsw = CorrectUser.UserCorrectAnsw;
+            //TotalAnsw = CorrectUser.UserTotalAnsw;
+            if (nextquestion)
+            {
+                
+            }
+            else
+            {
+                if(QuestCorrectAnsw == BtnAnswer)
+                {
+                    AnswValid = true;
+                }
+            }
         }
+        public IActionResult OnPost()
+        {
+            nextquestion = true;
+            return RedirectToPage("/RevisionQuestions/RevisionQuestions", new
+            {
+                nextquestion,
+                Username
+            });
+        }
+
     }
 }
